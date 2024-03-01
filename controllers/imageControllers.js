e07239416ec8ffff519a19b55670db820213f122
Aakash-bhandari <aakashbhandari990@gmail.com>
2024-03-01 16:39:53 +0530

    Image URl Updated

--- controllers/imageControllers.js
+++ controllers/imageControllers.js
@@ -1,7 +1,7 @@
 import grid from 'gridfs-stream';
 import mongoose from 'mongoose';
 // const url="https://blogapp-i0iw.onrender.com"
-const url = "http://localhost:8000";
+const url = "https://chatternet-backend.onrender.com";
 
 
 let gfs, gridfsBucket;
